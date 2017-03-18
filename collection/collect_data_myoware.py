180e3ccac25c4c1b4a8ccf2c4427c5d087caeee4
Pierpressure <pierpressure22@gmail.com>
2017-03-17 23:22:59 -0700

    Added collect_data scripts and raw_accelerometer data collected from belly

---
+++ collection/collect_data_myoware.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python2
+
+import serial
+import csv
+from datetime import datetime
+import time
+
+s = serial.Serial('/dev/cu.usbmodem1411',baudrate=115200)
+
+N_sensors = 3
+
+# tag = 'none'
+
+# header = ['timestamp', 'x', 'y', 'z', 'x_gyro', 'y_gyro', 'z_gyro', 'pressure']
+header = ['timestamp', 'x', 'y', 'z']
+
+date = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+fname = 'raw_data.csv'.format(date)
+
+print(fname)
+
+f_writer = open(fname, 'w')
+writer = csv.writer(f_writer)
+
+writer.writerow(header)
+
+def collect(length, tag=0):
+    ts = time.time()
+    while time.time() - ts < length:
+        td = time.time() - ts
+        if abs(td % 1) < 0.02:
+            print(td)
+        
+        try:
+            line = s.readline().decode('ascii').strip().split(',')
+            line = [float(x) for x in line]
+            # print(line)
+        except ValueError:
+            continue
+
+        if len(line) < N_sensors:
+            continue
+        
+        # row = line + [tag]
+        tstamp = str(time.time())
+        row = [tstamp] + line
+        writer.writerow(row)
+        f_writer.flush()
+
+
+print('starting in 3 seconds...')
+time.sleep(3)
+print('COLLECTING DATA')
+
+collect(30)
+
+f_writer.close()
