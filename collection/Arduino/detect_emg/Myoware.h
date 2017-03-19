6afad2cfdb6f377b5f50e29c242bcddd0370d48e
Pierre Karashchuk <krchtchk@gmail.com>
2017-03-18 17:56:55 -0700

    myoware test

--- collection/Arduino/detect_emg/Myoware.h
+++ collection/Arduino/detect_emg/Myoware.h
@@ -1,27 +1,18 @@
-#include "Arduino.h"
 #ifndef Myoware_h
 #define Myoware_h
+#include "Arduino.h"
+#include <Filters.h>
 
 class Myoware {
 public:
     Myoware(int thePin);
-    float getValue();
-    float processSignal();
+    float getFlex();
+    float getRaw();
 private:
     int pin;
 
-    int sensorValue;
-    float sensorBack;
-
-// high passed signal (>150Hz)
-    float hpval;
-    float hpback1;
-    float hpback2;
-
-// low passed value
-    float lowval;
-    float lowback1;
-    float lowback2;
-}
+    FilterOnePole filterHighpass;
+    FilterTwoPole filterLowpass;
+};
 
 #endif
--- collection/Arduino/detect_emg/detect_emg.ino
+++ collection/Arduino/detect_emg/detect_emg.ino
@@ -1,42 +1,19 @@
+#include "Myoware.h"
+
+Myoware left(A0);
+Myoware right(A2);
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
 }
-
-
-int sensorValue = 0;
-float sensorBack = 0;
-
-// high passed signal (>150Hz)
-float hpval = 0;
-float hpback1 = 0;
-float hpback2 = 0;
-
-// low passed value
-float lowval = 0;
-float lowback1 = 0;
-float lowback2 = 0;
 
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
-  sensorBack = sensorValue;
-  sensorValue = analogRead(A0);
-
-  // high pass filter
-  hpback2 = hpback1;
-  hpback1 = hpval;
-
-  hpval = 0.4625973*sensorValue - 0.4625973*sensorBack - 0.07480541*hpval;
-
-  // low pass filter on absolute value
-  lowback2 = lowback1;
-  lowback1 = lowval;
-  lowval = 3.26112508e-05*(abs(hpval) + 2*abs(hpback1) + abs(hpback2)) + 1.98378282*lowback1 - 0.98391326*lowback2;
-
-  // print out the value you read:
-  Serial.println(lowval*100);
-  delay(3);        // delay in between reads for stability
+  Serial.print(left.getFlex()*50);
+  Serial.print(" ");
+  Serial.print(right.getFlex()*50);
+  Serial.println();
+  delay(5);
 }
